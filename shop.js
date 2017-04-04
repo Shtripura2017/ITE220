b649cd1d7852828c40807bb0b92b1d6042ecf5ea
DESKTOP-LUHVQJ9\Shuvra Tripura <shuvra.tripura2000@gmail.com>
2017-04-04 15:56:33 +0700

    Hello

---
+++ shop.js
@@ -0,0 +1,14 @@
+var customer name = "Shuvra";
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+var customer = document.getElelmentById("")
+customer.textContent = customerName;
+
+
+
